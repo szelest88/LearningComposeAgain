8d7e95e32e776bdde17b3f61e7c6ad4fee191143
Artur Majewski <artur.majewski@gecko-lab.com>
2025-03-03 15:49:47 +0100

    Fun with header animations

---
+++ app/src/main/java/com/artmaj/myapplication/CollapsingAppBarNestedScrollConnection.kt
@@ -0,0 +1,48 @@
+package com.artmaj.myapplication
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
+import androidx.compose.ui.input.nestedscroll.NestedScrollSource
+
+class CollapsingAppBarNestedScrollConnection : NestedScrollConnection {
+
+    var headerOffset: Float by mutableFloatStateOf(0f)
+        private set
+    var progress: Float by mutableFloatStateOf(1f)
+        private set
+
+    var maxHeight: Float by mutableFloatStateOf(50f)
+    var minHeight: Float by mutableFloatStateOf(0f)
+
+    override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
+        val delta = available.y
+        /**
+         *  when direction is negative, meaning scrolling downward,
+         *  we are not consuming delta but passing it for Node Consumption
+         */
+        if (delta >= 0f) {
+            return Offset.Zero
+        }
+        val newOffset = headerOffset + delta
+        val previousOffset = headerOffset
+        val heightDelta = -(maxHeight - minHeight)
+        headerOffset = if (heightDelta > 0) 0f else newOffset.coerceIn(heightDelta, 0f)
+        progress = 1f - headerOffset / -maxHeight
+        val consumed = headerOffset - previousOffset
+        return Offset(0f, consumed)
+    }
+
+    override fun onPostScroll(consumed: Offset, available: Offset, source: NestedScrollSource): Offset {
+        val delta = available.y
+        val newOffset = headerOffset + delta
+        val previousOffset = headerOffset
+        val heightDelta = -(maxHeight - minHeight)
+        headerOffset = if (heightDelta > 0) 0f else newOffset.coerceIn(heightDelta, 0f)
+        progress = 1f - headerOffset / -maxHeight
+        val consumedValue = headerOffset - previousOffset
+        return Offset(0f, consumedValue)
+    }
+}
--- app/src/main/java/com/artmaj/myapplication/MainActivity.kt
+++ app/src/main/java/com/artmaj/myapplication/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.artmaj.myapplication
 
+import android.annotation.SuppressLint
 import android.media.Image
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -18,6 +20,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.HorizontalDivider
@@ -28,9 +31,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.artmaj.myapplication.ui.theme.MyApplicationTheme
+
+val connection = CollapsingAppBarNestedScrollConnection() //initialing nestedScrollConnection here
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,74 +53,121 @@ class MainActivity : ComponentActivity() {
         val bookND2 = BookNonData("LOTR", "Tolkien", BookCondition.NEW)
         setContent {
             MyApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
-                        Greeting(
-                            name = "" + someBook.title +
-                                    ", is theSame = " + (someBook == theSameBook),
-                            modifier = Modifier.padding(innerPadding)
-                        )
-                        Greeting(
-                            name = "" + bookND1.title +
-                                    ", is theSame = " + (bookND1 == bookND2),
-                            modifier = Modifier.padding(innerPadding)
-                        )
-                        for(i in 0..20)
-                            SomeLayout(
+                Scaffold(modifier = Modifier.fillMaxSize()) { _ ->
+                    LazyColumn(
+                        modifier = Modifier.nestedScroll(connection)
+                            .fillMaxSize()
+                        //    .verticalScroll(state = rememberScrollState())
+                    )
+                    {
+                        var dupa = connection.progress * 20
+                        Log.d("dupa ? = ", "" + dupa)
+                        item {
+                            Greeting(
+                                name = "" + someBook.title +
+                                        ", is theSame = " + (someBook == theSameBook),
+                                modifier = Modifier.padding(
+                                    PaddingValues(
+                                        top  = 50.dp,
+                                        bottom = 50.dp,
+                                        start = (
+                                                if (!connection.progress.isNaN()) {
+                                                    connection.progress * 40
+                                                } else {
+                                                    0.0f
+                                                }).dp
+                                    )
+                                )
+                            )
+
+                        }
+                        item {
+                            Greeting(
                                 name = "" + bookND1.title +
                                         ", is theSame = " + (bookND1 == bookND2),
-                                modifier = Modifier.padding(PaddingValues(top = 15.dp, bottom = 15.dp,
-                                    start = 10.dp, end = 10.dp))
+                                modifier = Modifier.padding(
+                                    PaddingValues(
+                                        start = (
+                                                if (!connection.progress.isNaN()) {
+                                                    connection.progress * 20
+                                                } else {
+                                                    0.0f
+                                                }).dp
+                                    )
+                                )
                             )
+                        }
+                        for (i in 0..20)
+                            item {
+                                SomeLayout(
+                                    name = "" + bookND1.title +
+                                            ", is theSame = " + (bookND1 == bookND2),
+                                    modifier = Modifier.padding(
+                                        PaddingValues(
+                                            top = 15.dp, bottom = 15.dp,
+                                            start = 10.dp, end = 10.dp
+                                        )
+                                    )
+                                )
+                            }
                     }
                 }
             }
         }
-    }
 
 
-    fun getMedian(values: List<Int>): Float {
-        return if (values.size % 2 == 1)
-            (values.sorted()[(values.size - 1) / 2]).toFloat()
-        else
-            ((values.sorted()[values.size / 2] + values.sorted()[values.size / 2 - 1]) / 2.0f)
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Composable
-fun SomeLayout(name: String, modifier: Modifier = Modifier) {
-    Column(modifier = Modifier.background(Color.Cyan)) {
-
-        Row(modifier = Modifier.fillMaxWidth(1.0f), verticalAlignment = Alignment.CenterVertically) {
-            Text(
-                text = "Dupa $name!",
-                modifier = modifier
-            )
-            Box(modifier = Modifier.width(32.dp).height(32.dp).background(Color(200,240,200)))
-
+        fun getMedian(values: List<Int>): Float {
+            return if (values.size % 2 == 1)
+                (values.sorted()[(values.size - 1) / 2]).toFloat()
+            else
+                ((values.sorted()[values.size / 2] + values.sorted()[values.size / 2 - 1]) / 2.0f)
         }
-        HorizontalDivider(modifier = Modifier.padding(start = 12.dp),thickness = 1.dp,  color = Color.Red)
     }
-}
-@Preview(showBackground = true)
-@Composable
-fun SomeLayoutPreview() {
-    MyApplicationTheme {
-        SomeLayout("Android")
+
+    @Composable
+    fun Greeting(name: String, modifier: Modifier = Modifier) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
     }
-}
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
+
+    @Composable
+    fun SomeLayout(name: String, modifier: Modifier = Modifier) {
+        Column(modifier = Modifier.background(Color.Cyan)) {
+
+            Row(
+                modifier = Modifier.fillMaxWidth(1.0f),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Dupa $name!",
+                    modifier = modifier
+                )
+                Box(modifier = Modifier.width(32.dp).height(32.dp).background(Color(200, 240, 200)))
+
+            }
+            HorizontalDivider(
+                modifier = Modifier.padding(start = 12.dp),
+                thickness = 1.dp,
+                color = Color.Red
+            )
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun SomeLayoutPreview() {
+        MyApplicationTheme {
+            SomeLayout("Android")
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        MyApplicationTheme {
+            Greeting("Android")
+        }
     }
 }
