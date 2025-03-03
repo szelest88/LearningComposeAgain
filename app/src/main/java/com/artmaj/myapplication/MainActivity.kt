b3e72c50035ba35f7b78c0010ec58e808508339a
Artur Majewski <artur.majewski@gecko-lab.com>
2025-03-03 13:32:44 +0100

    WIP fun with compose

--- app/src/main/java/com/artmaj/myapplication/MainActivity.kt
+++ app/src/main/java/com/artmaj/myapplication/MainActivity.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.gestures.ScrollableState
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -28,7 +32,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column {
+                    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
                         Greeting(
                             name = "" + someBook.title +
                                     ", is theSame = " + (someBook == theSameBook),
@@ -39,6 +43,12 @@ class MainActivity : ComponentActivity() {
                                     ", is theSame = " + (bookND1 == bookND2),
                             modifier = Modifier.padding(innerPadding)
                         )
+                        for(i in 0..100)
+                            SomeLayout(
+                                name = "" + bookND1.title +
+                                        ", is theSame = " + (bookND1 == bookND2),
+                                modifier = Modifier.padding(innerPadding)
+                            )
                     }
                 }
             }
@@ -62,6 +72,20 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+@Composable
+fun SomeLayout(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Dupa $name!",
+        modifier = modifier
+    )
+}
+@Preview(showBackground = true)
+@Composable
+fun SomeLayoutPreview() {
+    MyApplicationTheme {
+        SomeLayout("Android")
+    }
+}
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
