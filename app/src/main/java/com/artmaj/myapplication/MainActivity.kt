0309a089026e8a4b282f10bf7d7c4881a86ecdec
Artur Majewski <artur.majewski@gecko-lab.com>
2025-03-04 18:54:09 +0100

    WIP tak se grzebię

--- app/src/main/java/com/artmaj/myapplication/MainActivity.kt
+++ app/src/main/java/com/artmaj/myapplication/MainActivity.kt
@@ -11,21 +11,20 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.ScrollableState
 import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -34,13 +33,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.artmaj.myapplication.ui.theme.MyApplicationTheme
 
-val connection = CollapsingAppBarNestedScrollConnection() //initialing nestedScrollConnection here
+val connection: CollapsingAppBarNestedScrollConnection = CollapsingAppBarNestedScrollConnection() //initialing nestedScrollConnection here
 
 
 
@@ -59,36 +57,37 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyApplicationTheme {
                 Column {
-                    Greeting(
-                        name = "Hm" + someBook.title +
-                                ", is theSame = " + (someBook == theSameBook),
-                        modifier = Modifier
-                            .background(Color.LightGray)
-                            .padding(
-                                PaddingValues(
-                                    top = (10 +
+                    Row(horizontalArrangement = Arrangement.End) {
+                        Spacer(Modifier.weight(1.0f))
+                        Greeting(
+                            name = "??",
+                            modifier = Modifier
+                                .background(Color.LightGray)
+                                .padding(
+                                    PaddingValues(
+                                        top = (10 +
 
-                                            if (!connection.progress.isNaN() && connection.progress >= 0) {
-                                                15 + connection.progress * 20
-                                            } else {
-                                                15.0f
-                                            }).dp,
-                                    bottom = (10 +
+                                                if (!connection.progress.isNaN() && connection.progress >= 0) {
+                                                    15 + connection.progress * 20
+                                                } else {
+                                                    15.0f
+                                                }).dp,
+                                        bottom = (10 +
 
-                                            if (!connection.progress.isNaN() && connection.progress >= 0) {
-                                                connection.progress * 20
-                                            } else {
-                                                0.0f
-                                            }).dp,
-                                    start = 0.dp,
-                                    end = 0.dp
+                                                if (!connection.progress.isNaN() && connection.progress >= 0) {
+                                                    connection.progress * 20
+                                                } else {
+                                                    0.0f
+                                                }).dp,
+                                        start = 0.dp,
+                                        end = 0.dp
+                                    )
                                 )
-                            )
-                    )
-
+                        )
+                    }
                     Scaffold(modifier = Modifier
-                        .fillMaxSize()
-                        .background(Color.Cyan)) { _ ->
+                            .fillMaxSize()
+                            .background(Color.Cyan)) { _ ->
                         LazyColumn(
                             modifier = Modifier
                                 .nestedScroll(connection)
@@ -96,8 +95,7 @@ class MainActivity : ComponentActivity() {
                             //    .verticalScroll(state = rememberScrollState())
                         )
                         {
-                            var dupa = connection.progress * 20
-                            Log.d("dupa ? = ", "" + dupa)
+
                             stickyHeader {
                                 Greeting(
                                     name = "?" + someBook.title +
@@ -135,42 +133,27 @@ class MainActivity : ComponentActivity() {
                                     )
                                 )
                             }
-                            stickyHeader {
+                            stickyHeader { // does not work?
                                 Box(
                                     modifier = Modifier.background(color = Color.Black
                                     )
                                         .clip(
                                             RoundedCornerShape(
-                                                topStart = 20.dp,
+                                                topStart = 25.dp,
                                                 topEnd = 20.dp
                                             )
                                         ).height(150.dp).fillMaxWidth(1.0f),
 
 
-                                    content = { Text(text="dajdiasod", color = Color.White)}
+                                    content = { Text(text="Second sticky header?", color = Color.White)}
                                 )
                             }
-                            for (i in 0..20)
-                                if(i==3){
-                                    item {
-                                        SomeLayout(
-                                            name = "" + bookND1.title +
-                                                    ", is theSame = " + (bookND1 == bookND2),
-                                            modifier = Modifier.padding(
-                                                PaddingValues(
-                                                    top = 15.dp, bottom = 15.dp,
-                                                    start = 10.dp, end = 10.dp
-                                                )
-                                            ).background(Color.White)
-                                        )
-                                    }
+                            for (i in 0..40)
 
-                                }
-                                else if (i == 0) {
-                                    stickyHeader {
+                                if (i == 0) {
+                                    stickyHeader { // works
                                         SomeLayoutRounded(
-                                            name = "" + bookND1.title +
-                                                    ", is theSame = " + (bookND1 == bookND2),
+                                            name = "here we have some sticky header",
                                             modifier = Modifier
                                                 .padding(
                                                     PaddingValues(
