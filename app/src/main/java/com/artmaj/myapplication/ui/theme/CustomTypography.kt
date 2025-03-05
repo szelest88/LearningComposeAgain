07259d0872597b688dd29ac036d9373666eeda0e
Artur Majewski <artur.majewski@gecko-lab.com>
2025-03-05 13:43:51 +0100

    wip

---
+++ app/src/main/java/com/artmaj/myapplication/ui/theme/CustomTypography.kt
@@ -0,0 +1,43 @@
+package com.artmaj.myapplication.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.artmaj.myapplication.R
+
+// Set of Material typography styles to start with
+object  CustomTypography{
+
+    val SmallInfo = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontSize = 16.sp,
+        fontWeight = FontWeight.SemiBold,
+        lineHeight = 18.sp
+    )
+    val Heading1 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontSize = 36.sp,
+        fontWeight = FontWeight.SemiBold,
+        lineHeight = 48.sp
+    )
+}
+    /* Other default text styles to override
+    titleLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    )
+    */
